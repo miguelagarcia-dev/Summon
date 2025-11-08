dcd269541a71f17326da1497882d8cf73876a79f
Miguel Garcia <miguelagarciawork@gmail.com>
2025-11-08 15:24:02 -0500

    Initial Commit

---
+++ Summon/SummonApp.swift
@@ -0,0 +1,17 @@
+//
+//  SummonApp.swift
+//  Summon
+//
+//  Created by Miguel Garcia on 11/8/25.
+//
+
+import SwiftUI
+
+@main
+struct SummonApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
